0ff7c8a72c1c19a9922ce59961878cf808be2891
Tyson L. Swetnam <tyson-swetnam@users.noreply.github.com>
2022-04-12 15:49:36 +0000

    updated geospatial environment.yml

--- latest/entry.sh
+++ latest/entry.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo '{"irods_host": "data.cyverse.org", "irods_port": 1247, "irods_user_name": "$IPLANT_USER", "irods_zone_name": "iplant"}' | envsubst > $HOME/.irods/irods_environment.json
-
+python -m ipykernel install --user --name=geospatial --display-name "Python (Geospatial)" 
 exec jupyter lab --no-browser 
