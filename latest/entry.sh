7d344678ed5c42f16129f89bed2c2c15177b25da
Tyson L. Swetnam <tyson-swetnam@users.noreply.github.com>
2024-05-08 11:49:18 -0700

    Update entry.sh

--- latest/entry.sh
+++ latest/entry.sh
@@ -2,4 +2,7 @@
 
 echo '{"irods_host": "data.cyverse.org", "irods_port": 1247, "irods_user_name": "$IPLANT_USER", "irods_zone_name": "iplant"}' | envsubst > $HOME/.irods/irods_environment.json
 
+# Copy .gitconfig from volume mount
+cp ~/data-store/iplant/home/{$IPLANT_USER}/.gitconfig ~/.gitconfig
+
 exec jupyter lab --no-browser --LabApp.token="" --LabApp.password=""
