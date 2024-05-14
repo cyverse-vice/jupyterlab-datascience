50950e5b8187e517a6cfbbe13335a433e962cdfa
Tyson L. Swetnam <tyson-swetnam@users.noreply.github.com>
2024-05-14 00:46:14 +0000

    updated latest w/ rstudio proxy mess

--- latest/entry.sh
+++ latest/entry.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-echo '{"irods_host": "data.cyverse.org", "irods_port": 1247, "irods_user_name": "$IPLANT_USER", "irods_zone_name": "iplant"}' | envsubst > $HOME/.irods/irods_environment.json
+echo '{"irods_host": "data.cyverse.org", "irods_port": 1247, "irods_user_name": "$IPLANT_USER", "irods_zone_name": "iplant"}' >> $HOME/.irods/irods_environment.json
+
+echo "export PATH=$PATH:/opt/conda/bin" >> ~/.bashrc
 
 if [ -f /data-store/iplant/home/$IPLANT_USER/.gitconfig ]; then
   cp /data-store/iplant/home/$IPLANT_USER/.gitconfig ~/
