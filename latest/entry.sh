4446d3e522c761d7e7b43116896771476417aece
Tyson L. Swetnam <tyson-swetnam@users.noreply.github.com>
2025-12-10 21:52:22 +0000

    added agentic ai claude, codex, gemini

--- latest/entry.sh
+++ latest/entry.sh
@@ -6,7 +6,8 @@ touch $HOME/.irods/irods_environment.json
 echo '{"irods_host": "data.cyverse.org", "irods_port": 1247, "irods_user_name": "$IPLANT_USER", "irods_zone_name": "iplant"}' >> $HOME/.irods/irods_environment.json
 
 # Add conda to PATH
-echo "export PATH=$PATH:/opt/conda/bin" >> ~/.bashrc
+echo "npm config set prefix /home/jovyan/.npm-global" && \
+echo "export PATH=/home/jovyan/.npm-global/bin:/opt/conda/bin:$PATH" >> ~/.bashrc 
 
 # Copy configuration files from data store (legacy method)
 if [ -f /data-store/iplant/home/$IPLANT_USER/.gitconfig ]; then
